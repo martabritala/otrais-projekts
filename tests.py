951b7e3cc778d6aea3bb49b9015d87fae271701e
mgedra <madara.gedra@gmail.com>
2024-03-19 14:21:37 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,13 +1,14 @@
-pyton -m pip install flask
-        Flask
+
+
 from flask import Flask
 
 app = Flask(__name__)
+
 @app.route("/")
 def index():
     return "Sveiki!"
 
-    if __name__ =='__main__':
-            @app.run(port = 5000)
-            
+if __name__ =='__main__':
+    app.run(port = 5000)
+
 print("Sveiki!")
